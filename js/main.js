4fcf6605b5f6f74797839c525989157bdcf72d1a
erikleon <ekarwatowski@gmail.com>
2017-02-26 00:55:08 -0500

    js updates, binding slick and overlay, smooth scroll on load

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@
   var moomjy_js = {
     init: function () {
 
-
       moomjy_js.carouselHandler();
-      moomjy_js.scrollmagic();
       moomjy_js.carouselInit();
+
+      moomjy_js.overlayHandler();
+
       moomjy_js.clickHandler();
+
+      moomjy_js.smoothScroll(window.location.hash);
     },
     clickHandler: function () {
       // Toggle the navigation on click of hamburger menu class
@@ -16,18 +19,15 @@
       });
       // Animate scroll to top on click of top element
       $('a').on('click', function(event) {
-        console.log('click');
-        console.log($(this).prop('href'));
-        if (/#/.test($(this).prop('href'))) {
-          console.log('hash');
+        if (/\/#/.test($(this).prop('href')) && window.location.pathname === '/') {
           event.preventDefault();
           moomjy_js.smoothScroll($(this).prop('hash'));
+          window.history.pushState({},"", $(this).prop('href'));
         }
       });
 
-      $('.item.slick-active img').on('click', function() {
-        console.log($(this))
-        console.log('hehjejhej');
+      $('.overlay .close').on('click', function() {
+        moomjy_js.overlayToggle();
       });
 
     },
@@ -35,61 +35,44 @@
       $($this).toggleClass('active');
       $('nav .navigation-list').toggleClass('active');
     },
+    overlayToggle: function () {
+      $('.overlay').toggleClass('active');
+      $('body').toggleClass('noscroll');
+    },
+    videoHandler: function ($src) {
+      $('.overlay video').src = $src;
+      $('.overlay video').load();
+    },
+    overlayHandler: function () {
+      $('.item.slick-active img').on('click', function() {
+        moomjy_js.videoHandler($(this).data('src'));
+        moomjy_js.overlayToggle();
+      });
+    },
     scrollmagic: function () {
       var $controller = new ScrollMagic.Controller();
-      // var $pin = new ScrollMagic.Scene({
-      //       triggerElement: ".main",
-      //       triggerHook: 0,
-      //       duration: 0,
-      //       offset: 0,
-      //       loglevel: 3
-      //     })
-      //     .setPin("#top-nav")
-      //     // .addIndicators({name: "pin (duration: 0)"}) // add indicators (requires plugin)
-      //     .addTo($controller);
-      // var $scene = new ScrollMagic.Scene({
-      //     triggerElement: "#trigger-1",
-      //     triggerHook: 0,
-      //     duration: 0,
-      //     offset: 0,
-      //     reverse: true
-      //   })
-      //   .setTween("#top-nav", 1, {top: "20px"})
-      //   .addIndicators({name: "1 (duration: 63)"})
-      //   .addTo($controller);
-
-
     },
     smoothScroll: function ($this) {
-      console.log($($this));
-      $offset = $($this).offset();
-      console.log($offset);
-      $('html, body').animate({
-        scrollTop: $offset.top,
-      }, 1000);
+      if ($this) {
+        $offset = $($this).offset();
+        $('html, body').animate({
+          scrollTop: $offset.top,
+        }, 1000);
+      }
     },
     carouselHandler: function () {
       $('.carousel')
         .on('init', function(event, slick){
-            // let's do this after we init the banner slider
-
-            console.log('slider was initialized');
-            $('.slick-active').prev().addClass('prev');
-            $('.slick-active').next().addClass('next');
+          $('.slick-active').prev().addClass('prev');
+          $('.slick-active').next().addClass('next');
         })
         .on('beforeChange', function(event, slick, currentSlide, nextSlide){
-            // then let's do this before changing slides
-
-            console.log('before change');
-            $('.slick-slide').removeClass('prev next');
-
+          $('.slick-slide').removeClass('prev next');
         })
         .on('afterChange', function(event, slick, currentSlide, nextSlide){
-            // finally let's do this after changing slides
-
-            console.log('after change');
-            $('.slick-active').prev().addClass('prev');
-            $('.slick-active').next().addClass('next');
+          $('.slick-active').prev().addClass('prev');
+          $('.slick-active').next().addClass('next');
+          moomjy_js.overlayHandler();
         });
     },
     carouselInit: function () {
@@ -109,5 +92,4 @@
   };
   moomjy_js.init();
 
-  $(window).resize(moomjy_js.scrollmagic());
 })(jQuery, window, document);
